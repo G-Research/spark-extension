219bfa2e81acb81669eaac6c4e5ac3364b8604e2
Enrico Minack <github@enrico.minack.dev>
2022-03-18 13:08:27 +0100

    Add sorted group methods to Dataset

---
+++ src/main/scala/uk/co/gresearch/spark/group/package.scala
@@ -0,0 +1,79 @@
+package uk.co.gresearch.spark
+
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{Dataset, Encoder, Encoders}
+
+package object group {
+
+  case class SortedGroupByKeyValueDataset[K: Ordering : Encoder, V: Encoder, O](kv: Dataset[(K, V, O)],
+                                                                                partitions: Option[Int],
+                                                                                reverse: Boolean) {
+    implicit val encoder: Encoder[(K, V)] = Encoders.tuple(implicitly[Encoder[K]], implicitly[Encoder[V]])
+
+    private def keyColumn = col(kv.columns.head)
+    private def sortColumn = if (reverse) col(kv.columns.last).desc else col(kv.columns.last)
+
+    private val partitioned = partitions.map(parts => kv.repartition(parts, keyColumn)).getOrElse(kv.repartition(keyColumn))
+    private val ds = partitioned.sortWithinPartitions(keyColumn, sortColumn).map(v => (v._1, v._2))
+
+    def flatMapSortedGroups[W: Encoder](func: (K, Iterator[V]) => TraversableOnce[W]): Dataset[W] =
+      ds.mapPartitions(new GroupedIterator(_).flatMap(v => func(v._1, v._2)))
+
+    def flatMapSortedGroupsWithKeys[W: Encoder](func: (K, Iterator[V]) => TraversableOnce[W]): Dataset[(K, W)] = {
+      implicit val encoder: Encoder[(K, W)] = Encoders.tuple(implicitly[Encoder[K]], implicitly[Encoder[W]])
+      ds.mapPartitions(new GroupedIterator(_).flatMap(v => func(v._1, v._2).map(w => (v._1, w))))
+    }
+
+    def flatMapSortedGroups[S, W: Encoder](s: K => S)(f: (S, V) => TraversableOnce[W]): Dataset[W] = {
+      ds.mapPartitions(new GroupedIterator(_).flatMap { case (k, it) =>
+        val state = s(k)
+        it.flatMap(v => f(state, v))
+      })
+    }
+
+    def flatMapSortedGroupsWithKey[S, W: Encoder](s: K => S)(f: (S, V) => TraversableOnce[W]): Dataset[(K, W)] = {
+      implicit val encoder: Encoder[(K, W)] = Encoders.tuple(implicitly[Encoder[K]], implicitly[Encoder[W]])
+      ds.mapPartitions(new GroupedIterator(_).flatMap { case (k, it) =>
+        val state = s(k)
+        it.flatMap(v => f(state, v).map(w => (k, w)))
+      })
+    }
+
+  }
+
+  class GroupedIterator[K: Ordering, V](iter: Iterator[(K, V)]) extends Iterator[(K, Iterator[V])] {
+    private val values = iter.buffered
+    private var currentKey: Option[K] = None
+    private var currentGroup: Option[Iterator[V]] = None
+
+    override def hasNext: Boolean = {
+      if (currentGroup.isDefined) {
+        // consume current group
+        val it = currentGroup.get
+        while (it.hasNext) it.next
+      }
+
+      val isNext = values.hasNext
+      if (isNext) {
+        currentKey = Some(values.head._1)
+        currentGroup = Some(new GroupIterator(values))
+      } else {
+        currentKey = None
+        currentGroup = None
+      }
+      isNext
+    }
+
+    override def next(): (K, Iterator[V]) = (currentKey.get, currentGroup.get)
+  }
+
+  class GroupIterator[K: Ordering, V](iter: BufferedIterator[(K, V)]) extends Iterator[V] {
+    private val ordering = implicitly[Ordering[K]]
+    private val key = iter.head._1
+
+    override def hasNext: Boolean = iter.hasNext && ordering.compare(iter.head._1, key) == 0
+
+    override def next(): V = iter.next._2
+  }
+
+}
---
+++ src/test/scala/uk/co/gresearch/spark/TestSortedGroup.scala
@@ -0,0 +1,326 @@
+/*
+ * Copyright 2022 G-Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.co.gresearch.spark
+
+import org.apache.spark.sql.{Dataset, Row}
+import org.scalatest.funspec.AnyFunSpec
+
+case class Val(id: Int, seq: Int, value: Double)
+case class State(key: Int) {
+  var sum: Int = key
+
+  def add(i: Int): Int = {
+    sum = sum + i
+    sum
+  }
+}
+
+class TestSortedGroup extends AnyFunSpec with SparkTestSession {
+
+  import spark.implicits._
+
+  val ds: Dataset[Val] = Seq(
+    Val(1, 1, 1.1),
+    Val(1, 2, 1.2),
+    Val(1, 3, 1.3),
+
+    Val(2, 1, 2.1),
+    Val(2, 2, 2.2),
+    Val(2, 3, 2.3),
+
+    Val(3, 1, 3.1),
+  ).toDS().repartition(4).cache()
+
+  describe("test dataset") {
+    it("is randomly partitioned") {
+      val partitions: Array[List[Val]] = ds.mapPartitions(it => Iterator(it.toList)).collect()
+      assert(partitions === Array(
+        List(Val(3, 1, 3.1), Val(2, 1, 2.1)),
+        List(Val(1, 3, 1.3), Val(2, 3, 2.3)),
+        List(Val(2, 2, 2.2)),
+        List(Val(1, 2, 1.2), Val(1, 1, 1.1))
+      ))
+    }
+  }
+
+  describe("ds.groupByKeySorted") {
+
+    it("should flatMapSortedGroups") {
+      val actual = ds
+        .groupByKeySorted(v => v.id)(v => v.seq)
+        .flatMapSortedGroups((key, it) => it.zipWithIndex.map(v => (key, v._2, v._1)))
+        .collect()
+        .sortBy(v => (v._1, v._2))
+
+      val expected = Seq(
+        // (key, group index, value)
+        (1, 0, Val(1, 1, 1.1)),
+        (1, 1, Val(1, 2, 1.2)),
+        (1, 2, Val(1, 3, 1.3)),
+
+        (2, 0, Val(2, 1, 2.1)),
+        (2, 1, Val(2, 2, 2.2)),
+        (2, 2, Val(2, 3, 2.3)),
+
+        (3, 0, Val(3, 1, 3.1)),
+      )
+
+      assert(actual === expected)
+    }
+
+    it("should flatMapSortedGroupsWithKeys") {
+      val actual = ds
+        .groupByKeySorted(v => v.id)(v => v.seq)
+        .flatMapSortedGroupsWithKeys((key, it) => it.zipWithIndex.map(v => (key, v._2, v._1)))
+        .collect()
+        .sortBy(v => (v._1, v._2._1, v._2._2))
+
+      val expected = Seq(
+        // (key, (key, group index, value))
+        (1, (1, 0, Val(1, 1, 1.1))),
+        (1, (1, 1, Val(1, 2, 1.2))),
+        (1, (1, 2, Val(1, 3, 1.3))),
+
+        (2, (2, 0, Val(2, 1, 2.1))),
+        (2, (2, 1, Val(2, 2, 2.2))),
+        (2, (2, 2, Val(2, 3, 2.3))),
+
+        (3, (3, 0, Val(3, 1, 3.1))),
+      )
+
+      assert(actual === expected)
+    }
+
+    it("should flatMapSortedGroups with state") {
+      val actual = ds
+        .groupByKeySorted(v => v.id)(v => v.seq)
+        .flatMapSortedGroups(key => State(key))((state, v) => Iterator((v, state.add(v.seq))))
+        .collect()
+        .sortBy(v => (v._1.id, v._1.seq))
+
+      val expected = Seq(
+        // (value, state)
+        (Val(1, 1, 1.1), 1 + 1),
+        (Val(1, 2, 1.2), 1 + 1 + 2),
+        (Val(1, 3, 1.3), 1 + 1 + 2 + 3),
+
+        (Val(2, 1, 2.1), 2 + 1),
+        (Val(2, 2, 2.2), 2 + 1 + 2),
+        (Val(2, 3, 2.3), 2 + 1 + 2 + 3),
+
+        (Val(3, 1, 3.1), 3 + 1),
+      )
+
+      assert(actual === expected)
+    }
+
+    it("should flatMapSortedGroupsWithKey with state") {
+      val actual = ds
+        .groupByKeySorted(v => v.id)(v => v.seq)
+        .flatMapSortedGroupsWithKey(key => State(key))((state, v) => Iterator((v, state.add(v.seq))))
+        .collect()
+        .sortBy(v => (v._1, v._2._1.id, v._2._1.seq))
+
+      val expected = Seq(
+        // (key, (value, state))
+        (1, (Val(1, 1, 1.1), 1 + 1)),
+        (1, (Val(1, 2, 1.2), 1 + 1 + 2)),
+        (1, (Val(1, 3, 1.3), 1 + 1 + 2 + 3)),
+
+        (2, (Val(2, 1, 2.1), 2 + 1)),
+        (2, (Val(2, 2, 2.2), 2 + 1 + 2)),
+        (2, (Val(2, 3, 2.3), 2 + 1 + 2 + 3)),
+
+        (3, (Val(3, 1, 3.1), 3 + 1)),
+      )
+
+      assert(actual === expected)
+    }
+
+    /**
+     *  it("should flatMapSortedGroups with partition num") {
+     *  val actual = ds
+     *  .groupByKeySorted(key = v => v.id, partitions = Some(10))(v => v.seq)
+     *  .flatMapSortedGroups((key, it) => it.zipWithIndex.map(v => (key, v._2, v._1)))
+     *  .collect()
+     *  .sortBy(v => (v._1, v._2))
+     *
+     *  val expected = Seq(
+     *  // (key, group index, value)
+     *  (1, 0, Val(1, 1, 1.1)),
+     *  (1, 1, Val(1, 2, 1.2)),
+     *  (1, 2, Val(1, 3, 1.3)),
+     *
+     *  (2, 0, Val(2, 1, 2.1)),
+     *  (2, 1, Val(2, 2, 2.2)),
+     *  (2, 2, Val(2, 3, 2.3)),
+     *
+     *  (3, 0, Val(3, 1, 3.1)),
+     *  )
+     *
+     *  assert(actual === expected)
+     *  }
+     */
+    it("should flatMapSortedGroups reverse") {
+      val actual = ds
+        .groupByKeySorted(key = v => v.id)(order = v => v.seq, reverse = true)
+        .flatMapSortedGroups((key, it) => it.zipWithIndex.map(v => (key, v._2, v._1)))
+        .collect()
+        .sortBy(v => (v._1, v._2))
+
+      val expected = Seq(
+        // (key, group index, value)
+        (1, 0, Val(1, 3, 1.3)),
+        (1, 1, Val(1, 2, 1.2)),
+        (1, 2, Val(1, 1, 1.1)),
+
+        (2, 0, Val(2, 3, 2.3)),
+        (2, 1, Val(2, 2, 2.2)),
+        (2, 2, Val(2, 1, 2.1)),
+
+        (3, 0, Val(3, 1, 3.1)),
+      )
+
+      assert(actual === expected)
+    }
+
+    /**
+     *  it("should flatMapSortedGroups with partition num and reverse") {
+     *  val actual = ds
+     *  .groupByKeySorted(key = v => v.id, partitions = Some(10))(order = v => v.seq, reverse = true)
+     *  .flatMapSortedGroups((key, it) => it.zipWithIndex.map(v => (key, v._2, v._1)))
+     *  .collect()
+     *  .sortBy(v => (v._1, v._2))
+     *
+     *  val expected = Seq(
+     *  // (key, group index, value)
+     *  (1, 0, Val(1, 1, 1.1)),
+     *  (1, 1, Val(1, 2, 1.2)),
+     *  (1, 2, Val(1, 3, 1.3)),
+     *
+     *  (2, 0, Val(2, 1, 2.1)),
+     *  (2, 1, Val(2, 2, 2.2)),
+     *  (2, 2, Val(2, 3, 2.3)),
+     *
+     *  (3, 0, Val(3, 1, 3.1)),
+     *  )
+     *
+     *  assert(actual === expected)
+     *  }
+     */
+  }
+
+  describe("df.groupByKeySorted") {
+
+    val df = ds.toDF()
+
+    def valueRowToTuple(value: Row): (Int, Int, Double) = (value.getInt(0), value.getInt(1), value.getDouble(2))
+
+    it("should flatMapSortedGroups") {
+      val actual = df
+        .groupByKeySorted(v => v.getInt(0))(v => v.getInt(1))
+        .flatMapSortedGroups((key, it) => it.zipWithIndex.map(v => (key, v._2, valueRowToTuple(v._1))))
+        .collect()
+        .sorted
+
+      val expected = Seq(
+        // (key, group index, value)
+        (1, 0, (1, 1, 1.1)),
+        (1, 1, (1, 2, 1.2)),
+        (1, 2, (1, 3, 1.3)),
+
+        (2, 0, (2, 1, 2.1)),
+        (2, 1, (2, 2, 2.2)),
+        (2, 2, (2, 3, 2.3)),
+
+        (3, 0, (3, 1, 3.1)),
+      )
+
+      assert(actual === expected)
+    }
+
+    it("should flatMapSortedGroupsWithKeys") {
+      val actual = df
+        .groupByKeySorted(v => v.getInt(0))(v => v.getInt(1))
+        .flatMapSortedGroupsWithKeys((key, it) => it.zipWithIndex.map(v => (key, v._2, valueRowToTuple(v._1))))
+        .collect()
+        .sorted
+
+      val expected = Seq(
+        // (key, (key, group index, value))
+        (1, (1, 0, (1, 1, 1.1))),
+        (1, (1, 1, (1, 2, 1.2))),
+        (1, (1, 2, (1, 3, 1.3))),
+
+        (2, (2, 0, (2, 1, 2.1))),
+        (2, (2, 1, (2, 2, 2.2))),
+        (2, (2, 2, (2, 3, 2.3))),
+
+        (3, (3, 0, (3, 1, 3.1))),
+      )
+
+      assert(actual === expected)
+    }
+
+    it("should flatMapSortedGroups with state") {
+      val actual = df
+        .groupByKeySorted(v => v.getInt(0))(v => v.getInt(1))
+        .flatMapSortedGroups(key => State(key))((state, v) => Iterator((valueRowToTuple(v), state.add(v.getInt(1)))))
+        .collect()
+        .sorted
+
+      val expected = Seq(
+        // (value, state)
+        ((1, 1, 1.1), 1 + 1),
+        ((1, 2, 1.2), 1 + 1 + 2),
+        ((1, 3, 1.3), 1 + 1 + 2 + 3),
+
+        ((2, 1, 2.1), 2 + 1),
+        ((2, 2, 2.2), 2 + 1 + 2),
+        ((2, 3, 2.3), 2 + 1 + 2 + 3),
+
+        ((3, 1, 3.1), 3 + 1),
+      )
+
+      assert(actual === expected)
+    }
+
+    it("should flatMapSortedGroupsWithKey with state") {
+      val actual = df
+        .groupByKeySorted(v => v.getInt(0))(v => v.getInt(1))
+        .flatMapSortedGroupsWithKey(key => State(key))((state, v) => Iterator((valueRowToTuple(v), state.add(v.getInt(1)))))
+        .collect()
+        .sorted
+
+      val expected = Seq(
+        // (key, (value, state))
+        (1, ((1, 1, 1.1), 1 + 1)),
+        (1, ((1, 2, 1.2), 1 + 1 + 2)),
+        (1, ((1, 3, 1.3), 1 + 1 + 2 + 3)),
+
+        (2, ((2, 1, 2.1), 2 + 1)),
+        (2, ((2, 2, 2.2), 2 + 1 + 2)),
+        (2, ((2, 3, 2.3), 2 + 1 + 2 + 3)),
+
+        (3, ((3, 1, 3.1), 3 + 1)),
+      )
+
+      assert(actual === expected)
+    }
+  }
+
+}
