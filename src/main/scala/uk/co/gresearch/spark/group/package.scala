ab1f519e17f3768c324be6500c0b607f9f38df04
Enrico Minack <github@enrico.minack.dev>
2022-04-07 15:35:04 +0200

    Fixing javadoc warnings, add javadoc to flatMapSortedGroups methods

--- src/main/scala/uk/co/gresearch/spark/group/package.scala
+++ src/main/scala/uk/co/gresearch/spark/group/package.scala
@@ -14,16 +14,48 @@ package object group {
    * and sorted within partitions by the key and value.
    *
    * @param ds the properly partitioned and sorted dataset
-   * @param ordering$K$0 ordering over the keys
-   * @param encoder$K$1 encoder for the keys
-   * @param encoder$V$0 encoder for the values
-   * @tparam K type of the keys
-   * @tparam V type of the values
+   * @tparam K type of the keys with ordering and encoder
+   * @tparam V type of the values with encoder
    */
   case class SortedGroupByDataset[K: Ordering : Encoder, V: Encoder] private (ds: Dataset[(K, V)]) {
+    /**
+     * (Scala-specific)
+     * Applies the given function to each group of data.  For each unique group, the function will
+     * be passed the group key and a sorted iterator that contains all of the elements in the group.
+     * The function can return an iterator containing elements of an arbitrary type which will be
+     * returned as a new [[Dataset]].
+     *
+     * This function does not support partial aggregation, and as a result requires shuffling all
+     * the data in the [[Dataset]]. If an application intends to perform an aggregation over each
+     * key, it is best to use the reduce function or an
+     * `org.apache.spark.sql.expressions#Aggregator`.
+     *
+     * Internally, the implementation will spill to disk if any given group is too large to fit into
+     * memory.  However, users must take care to avoid materializing the whole iterator for a group
+     * (for example, by calling `toList`) unless they are sure that this is possible given the memory
+     * constraints of their cluster.
+     */
     def flatMapSortedGroups[W: Encoder](func: (K, Iterator[V]) => TraversableOnce[W]): Dataset[W] =
       ds.mapPartitions(new GroupedIterator(_).flatMap(v => func(v._1, v._2)))
 
+    /**
+     * (Scala-specific)
+     * Applies the given function to each group of data.  For each unique group, the function s will
+     * be passed the group key to create a state instance, while the function func will be passed
+     * that state instance and group values in sequence according to the sort order in the groups.
+     * The function func can return an iterator containing elements of an arbitrary type which will
+     * be returned as a new [[Dataset]].
+     *
+     * This function does not support partial aggregation, and as a result requires shuffling all
+     * the data in the [[Dataset]]. If an application intends to perform an aggregation over each
+     * key, it is best to use the reduce function or an
+     * `org.apache.spark.sql.expressions#Aggregator`.
+     *
+     * Internally, the implementation will spill to disk if any given group is too large to fit into
+     * memory.  However, users must take care to avoid materializing the whole iterator for a group
+     * (for example, by calling `toList`) unless they are sure that this is possible given the memory
+     * constraints of their cluster.
+     */
     def flatMapSortedGroups[S, W: Encoder](s: K => S)(func: (S, V) => TraversableOnce[W]): Dataset[W] = {
       ds.mapPartitions(new GroupedIterator(_).flatMap { case (k, it) =>
         val state = s(k)
