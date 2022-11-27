d2d83a3b41b2a3514fbc4c3ad55c9db49ef41eab
Enrico Minack <github@enrico.minack.dev>
2022-11-27 11:21:14 +0100

    Implement absolute and relative epsilon diff comparator

--- src/main/scala/uk/co/gresearch/spark/diff/comparator/AbsoluteEpsilonDiffComparator.scala
+++ src/main/scala/uk/co/gresearch/spark/diff/comparator/AbsoluteEpsilonDiffComparator.scala
@@ -2,9 +2,31 @@ package uk.co.gresearch.spark.diff.comparator
 
 import scala.math.Equiv
 
-case class EpsilonDiffComparator(relativeEpsilon: Double) extends Equiv[Int] {
-  override def equiv(left: Int, right: Int): Boolean = {
-    val delta = math.max(math.abs(left), math.abs(right)) * relativeEpsilon
-    math.abs(left - right) <= delta
-  }
+trait AbsoluteEpsilonDiffComparator[T] extends Equiv[T]
+
+case class AbsoluteEpsilonIntDiffComparator(absEpsilon: Int) extends AbsoluteEpsilonDiffComparator[Int] {
+  override def equiv(left: Int, right: Int): Boolean =
+    math.abs(left - right) <= absEpsilon
+}
+
+case class AbsoluteEpsilonLongDiffComparator(absEpsilon: Long) extends AbsoluteEpsilonDiffComparator[Long] {
+  override def equiv(left: Long, right: Long): Boolean =
+    math.abs(left - right) <= absEpsilon
+}
+
+case class AbsoluteEpsilonFloatDiffComparator(absEpsilon: Float) extends AbsoluteEpsilonDiffComparator[Float] {
+  override def equiv(left: Float, right: Float): Boolean =
+    math.abs(left - right) <= absEpsilon
+}
+
+case class AbsoluteEpsilonDoubleDiffComparator(absEpsilon: Double) extends AbsoluteEpsilonDiffComparator[Double] {
+  override def equiv(left: Double, right: Double): Boolean =
+    math.abs(left - right) <= absEpsilon
+}
+
+object AbsoluteEpsilonDiffComparator {
+  def apply(relativeEpsilon: Int): AbsoluteEpsilonDiffComparator[Int] = AbsoluteEpsilonIntDiffComparator(relativeEpsilon)
+  def apply(relativeEpsilon: Long): AbsoluteEpsilonDiffComparator[Long] = AbsoluteEpsilonLongDiffComparator(relativeEpsilon)
+  def apply(relativeEpsilon: Float): AbsoluteEpsilonDiffComparator[Float] = AbsoluteEpsilonFloatDiffComparator(relativeEpsilon)
+  def apply(relativeEpsilon: Double): AbsoluteEpsilonDiffComparator[Double] = AbsoluteEpsilonDoubleDiffComparator(relativeEpsilon)
 }
--- src/main/scala/uk/co/gresearch/spark/diff/comparator/RelativeEpsilonDiffComparator.scala
+++ src/main/scala/uk/co/gresearch/spark/diff/comparator/RelativeEpsilonDiffComparator.scala
@@ -1,5 +1,40 @@
 package uk.co.gresearch.spark.diff.comparator
 
-class RelativeEpsilonDiffComparator {
+import scala.math.Equiv
 
+trait RelativeEpsilonDiffComparator[T] extends Equiv[T]
+
+case class RelativeEpsilonIntDiffComparator(relativeEpsilon: Double) extends RelativeEpsilonDiffComparator[Int] {
+  override def equiv(left: Int, right: Int): Boolean = {
+    val delta = math.max(math.abs(left), math.abs(right)) * relativeEpsilon
+    math.abs(left - right) <= delta
+  }
+}
+
+case class RelativeEpsilonLongDiffComparator(relativeEpsilon: Double) extends RelativeEpsilonDiffComparator[Long] {
+  override def equiv(left: Long, right: Long): Boolean = {
+    val delta = math.max(math.abs(left), math.abs(right)) * relativeEpsilon
+    math.abs(left - right) <= delta
+  }
+}
+
+case class RelativeEpsilonFloatDiffComparator(relativeEpsilon: Double) extends RelativeEpsilonDiffComparator[Float] {
+  override def equiv(left: Float, right: Float): Boolean = {
+    val delta = math.max(math.abs(left), math.abs(right)) * relativeEpsilon
+    math.abs(left - right) <= delta
+  }
+}
+
+case class RelativeEpsilonDoubleDiffComparator(relativeEpsilon: Double) extends RelativeEpsilonDiffComparator[Double] {
+  override def equiv(left: Double, right: Double): Boolean = {
+    val delta = math.max(math.abs(left), math.abs(right)) * relativeEpsilon
+    math.abs(left - right) <= delta
+  }
+}
+
+object RelativeEpsilonDiffComparator {
+  def forInt(relativeEpsilon: Double): RelativeEpsilonDiffComparator[Int] = RelativeEpsilonIntDiffComparator(relativeEpsilon)
+  def forLong(relativeEpsilon: Double): RelativeEpsilonDiffComparator[Long] = RelativeEpsilonLongDiffComparator(relativeEpsilon)
+  def forFloat(relativeEpsilon: Double): RelativeEpsilonDiffComparator[Float] = RelativeEpsilonFloatDiffComparator(relativeEpsilon)
+  def forDouble(relativeEpsilon: Double): RelativeEpsilonDiffComparator[Double] = RelativeEpsilonDoubleDiffComparator(relativeEpsilon)
 }
