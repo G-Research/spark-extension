0cb6754776a2ba10d2767c7356cd07b07fe0a6cd
Enrico Minack <github@enrico.minack.dev>
2023-03-31 23:30:54 +0200

    Fix python tests

--- python/test/test_parquet.py
+++ python/test/test_parquet.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from pathlib import Path
 
 from spark_common import SparkTest
 import gresearch.spark.parquet
@@ -18,20 +19,22 @@ import gresearch.spark.parquet
 
 class ParquetTest(SparkTest):
 
+    test_file = str((Path(__file__).parent.parent.parent / "src" / "test" / "files" / "test.parquet").resolve())
+
     def test_parquet_metadata(self):
-        self.spark.read.parquet_metadata("../../../spark-dgraph-connector/dgraph.dbpedia.rdf-label.parquet").show(truncate=False)
+        self.assertEqual(self.spark.read.parquet_metadata(self.test_file).count(), 2)
 
     def test_parquet_blocks(self):
-        self.spark.read.parquet_blocks("../../../spark-dgraph-connector/dgraph.dbpedia.rdf-label.parquet").show(truncate=False)
+        self.assertEqual(self.spark.read.parquet_blocks(self.test_file).count(), 3)
 
     def test_parquet_block_columns(self):
-        self.spark.read.parquet_block_columns("../../../spark-dgraph-connector/dgraph.dbpedia.rdf-label.parquet").show(truncate=False)
+        self.assertEqual(self.spark.read.parquet_block_columns(self.test_file).count(), 6)
 
     def test_parquet_partitions(self):
-        self.spark.read.parquet_partitions("../../../spark-dgraph-connector/dgraph.dbpedia.rdf-label.parquet").show(truncate=False)
+        self.assertEqual(self.spark.read.parquet_partitions(self.test_file).count(), 2)
 
     def test_parquet_partition_rows(self):
-        self.spark.read.parquet_partition_rows("../../../spark-dgraph-connector/dgraph.dbpedia.rdf-label.parquet").show(truncate=False)
+        self.assertEqual(self.spark.read.parquet_partition_rows(self.test_file).count(), 2)
 
 
 if __name__ == '__main__':
