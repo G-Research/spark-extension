4c8dbb7a7cabe4ab85eb7bc8af62ae1bd6a19ae4
Enrico Minack <github@enrico.minack.dev>
2023-04-01 11:41:54 +0200

    Use links for identical classes

--- src/main/scala-spark-3.0/uk/co/gresearch/spark/parquet/SplitFile.scala
+++ src/main/scala-spark-3.0/uk/co/gresearch/spark/parquet/SplitFile.scala
@@ -1,9 +1,1 @@
-package uk.co.gresearch.spark.parquet
-
-import org.apache.spark.sql.execution.datasources.PartitionedFile
-
-case class SplitFile(filePath: String, start: Long, length: Long, fileSize: Option[Long])
-
-object SplitFile {
-  def apply(file: PartitionedFile): SplitFile = SplitFile(file.filePath, file.start, file.length, None)
-}
+../../../../../../scala-spark-3.2/uk/co/gresearch/spark/parquet/SplitFile.scala
