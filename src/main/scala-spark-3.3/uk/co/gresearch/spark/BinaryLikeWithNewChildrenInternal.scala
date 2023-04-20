56435ea7fad2f8207dd90f2529a8835ac8d90a6c
Enrico Minack <github@enrico.minack.dev>
2023-04-20 16:52:38 +0200

    Make Spark 3.4 the main version, remove last traces of 3.0 and 3.1

--- src/main/scala-spark-3.3/uk/co/gresearch/spark/BinaryLikeWithNewChildrenInternal.scala
+++
@@ -1,7 +0,0 @@
-package uk.co.gresearch.spark
-
-/**
- * Spark version specific trait that back-ports BinaryLike[T].withNewChildrenInternal(T, T)
- * to Spark 3.0 and 3.1. This is empty in Spark 3.2 and beyond.
- */
-trait BinaryLikeWithNewChildrenInternal[T]
