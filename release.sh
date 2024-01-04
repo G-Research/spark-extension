d88045d03767556e57cf74744ce4142591013413
Enrico Minack <github@enrico.minack.dev>
2024-01-04 15:06:00 +0100

    Upgrade Spark versions in relese.sh

--- release.sh
+++ release.sh
@@ -108,13 +108,13 @@ mkdir -p python/pyspark/jars/
 ./set-version.sh 3.0.3 2.12.10; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
 ./set-version.sh 3.1.3 2.12.10; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
 ./set-version.sh 3.2.4 2.12.15; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
-./set-version.sh 3.3.3 2.12.15; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
-./set-version.sh 3.4.1 2.12.17; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
+./set-version.sh 3.3.4 2.12.15; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
+./set-version.sh 3.4.2 2.12.17; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
 ./set-version.sh 3.5.0 2.12.18; mvn clean deploy -Dsign; mvn nexus-staging:release; ./build-whl.sh
 
 ./set-version.sh 3.2.4 2.13.5; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.3.3 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
-./set-version.sh 3.4.1 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
+./set-version.sh 3.3.4 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
+./set-version.sh 3.4.2 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
 ./set-version.sh 3.5.0 2.13.8; mvn clean deploy -Dsign; mvn nexus-staging:release
 
 # upload to test PyPi
