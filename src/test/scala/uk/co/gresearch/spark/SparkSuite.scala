77e8275891a6305999406f741f0b11000c7ff718
Enrico Minack <github@enrico.minack.dev>
2024-03-20 11:46:00 +0100

    Account for changed exception message in Spark 4 (#234)

--- src/test/scala/uk/co/gresearch/spark/SparkSuite.scala
+++ src/test/scala/uk/co/gresearch/spark/SparkSuite.scala
@@ -666,6 +666,7 @@ class SparkSuite extends AnyFunSuite with SparkTestSession {
         Seq(1L).toDF("ts").select(timestampToDotNetTicks($"ts")).collect()
       }.getMessage
 
+      // SparkMajorVersion == 2 no supported
       if (SparkMajorVersion == 3 && SparkMinorVersion == 1) {
         assert(
           message.startsWith(
@@ -678,10 +679,16 @@ class SparkSuite extends AnyFunSuite with SparkTestSession {
             "cannot resolve 'unix_micros(ts)' due to data type mismatch: argument 1 requires timestamp type, however, 'ts' is of bigint type.;"
           )
         )
-      } else if (SparkMajorVersion == 3 && SparkMinorVersion >= 4 || SparkMajorVersion > 3) {
+      } else if (SparkMajorVersion == 3 && SparkMinorVersion >= 4) {
         assert(
           message.startsWith(
             "[DATATYPE_MISMATCH.UNEXPECTED_INPUT_TYPE] Cannot resolve \"unix_micros(ts)\" due to data type mismatch: Parameter 1 requires the \"TIMESTAMP\" type, however \"ts\" has the type \"BIGINT\"."
+          )
+        )
+      } else { // SparkMajorVersion > 3
+        assert(
+          message.startsWith(
+            "[DATATYPE_MISMATCH.UNEXPECTED_INPUT_TYPE] Cannot resolve \"unix_micros(ts)\" due to data type mismatch: The first parameter requires the \"TIMESTAMP\" type, however \"ts\" has the type \"BIGINT\"."
           )
         )
       }
