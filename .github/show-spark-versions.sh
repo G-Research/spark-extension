bcc00ca29162090469e6b535d50269965dc4be5c
Enrico Minack <github@enrico.minack.dev>
2026-01-19 22:39:59 +0100

    Upgrade and align Spark versions across all workflows (#339)

---
+++ .github/show-spark-versions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+base=$(cd "$(dirname "$0")"; pwd)
+
+grep -- "-version" "$base"/workflows/prime-caches.yml | sed -e "s/ -//g" -e "s/ //g" -e "s/'//g" | grep -v -e "matrix" -e "]" | while read line
+do
+  IFS=":" read var compat_version <<< "$line"
+  if [[ "$var" == "spark-compat-version" ]]
+  then
+    while read line
+    do
+      IFS=":" read var patch_version <<< "$line"
+      if [[ "$var" == "spark-patch-version" ]]
+      then
+        echo -n "spark-version: $compat_version.$patch_version"
+        read line
+        if [[ "$line" == "spark-snapshot-version:true" ]]
+        then
+          echo "-SNAPSHOT"
+        else
+          echo
+        fi
+        break
+      fi
+    done
+  fi
+done > "$base"/workflows/prime-caches.yml.tmp
+
+grep spark-version "$base"/workflows/*.yml "$base"/workflows/prime-caches.yml.tmp | cut -d : -f 2- | sed -e "s/^[ -]*//" -e "s/'//g" | grep "^spark-version" | grep -v "matrix" | sort | uniq
+
